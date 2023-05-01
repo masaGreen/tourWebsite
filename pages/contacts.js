2a4560921612c985ec34b10a0a7f10ce32c76bf9
Masaa <davidmachariamj@gmail.com>
2023-05-01 20:31:19 +0300

    footers

--- pages/contacts.js
+++ pages/contacts.js
@@ -7,7 +7,7 @@ export default function contacts(){
     <div className={styles.card}>
         
           <div className={styles.cardDescription}>
-            <h3 style={{fontSize:"1rem", fontFamily:"cursive",padding:"0.5rem"}}>You can reach us through email <strong style={{color:"blue"}}>greenprinttours@gmail.com</strong> or through our social media pages.</h3>
+            <h3 style={{fontSize:"1rem", fontFamily:"arial",padding:"0.5rem"}}>You can reach us through email <strong style={{color:"blue"}}>greenprinttours@gmail.com</strong> or through our social media pages.</h3>
             <div className={styles.medialinks}>
             <div><Link href="https://wa.me/254716794207"><FontAwesomeIcon icon={faWhatsapp} style={{color: "white",background:"rgb(11, 158, 11)", height:"3rem", width:"3rem", borderRadius:"50%"}} /></Link></div>  
                 <div><Link href="/"><FontAwesomeIcon icon={faFacebook} style={{color:"rgb(0, 110, 255)", background:"white",border:"none", outline:"none",height:"3rem", width:"3rem", borderRadius:"50%"}} /></Link></div>  
